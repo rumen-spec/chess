ce84aa5657ae9bfe1b5094d86f57665e1a9e5f46
rumen <osarumen668@gmail.com>
2024-11-17 18:14:54 -0500

    Update consts.ts

--- frontend/src/pages/consts.ts
+++ frontend/src/pages/consts.ts
@@ -1,1 +1,12 @@
 export const BACKEND = 'ws://localhost:8080'
+
+
+const scores = new Map<string, number>()
+
+scores.set('P', 1);
+scores.set('B', 3);
+scores.set('N', 3);
+scores.set('R', 5);
+scores.set('Q', 9);
+
+export default scores;
