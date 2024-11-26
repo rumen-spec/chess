49aacc53085966f4009fc99bb1a8ff358534cc06
rumen <osarumen668@gmail.com>
2024-11-26 17:00:42 -0500

    Update Tile.tsx

--- frontend/src/pages/Tile.tsx
+++ frontend/src/pages/Tile.tsx
@@ -1,6 +1,4 @@
 import "./Tile.css";
-import resources from "./consts.ts";
-
 
 interface Props{
     num: number;
@@ -9,16 +7,15 @@ interface Props{
 }
 
 export default function Tile({num, id, image}: Props) {
-    const {images} = resources;
 
 
-    if(num%2==0){
+     if(num%2==0){
         return(<div className='tile black-tile' id={id} >
-            {image !== ''? <div style={{backgroundImage: `url(${images.get(image)})`}} className='chess-piece' id={id} key={image}></div> : <></>}
+            {image !== ''? <div style={{backgroundImage: `url("../../images/${image}.png")`}} className='chess-piece' id={id} key={image}></div> : <></>}
         </div>)
     }if(num%2==1){
         return(<div className='tile white-tile' id={id} >
-            {image !== ''? <div style={{backgroundImage: `url(${images.get(image)})`}} className='chess-piece' id={id} key={image}></div> : <></>}
+            {image !== ''? <div style={{backgroundImage: `url("../../images/${image}.png")`}} className='chess-piece' id={id} key={image}></div> : <></>}
         </div>)
     }
 
