d3e63baed400ff81e1d45979b3b50a8af14437fa
rumen <osarumen668@gmail.com>
2024-12-02 19:46:58 -0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Game from "./pages/Game.tsx";
 import {WebSocketProvider} from "./pages/WebSocketContext.tsx";
 import Home from "./pages/Home.tsx";
-import {ProtectedRoute} from "./pages/ProtectedRoute.tsx";
 
 function App() {
     return(
